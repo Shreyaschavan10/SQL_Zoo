d5edcb8413641d8b1973c61a50526c59482d43b8
Shreyaschavan10 <166787514+Shreyaschavan10@users.noreply.github.com>
2025-01-19 17:00:47 +0530

    Solved Problems

---
+++ select_from/Select_basic.sql
@@ -0,0 +1,3 @@
+-- Que 1
+-- The example uses a WHERE clause to show the population of 'Germany'. Note that strings should be in 'single quotes';
+select population from world where name = 'Germany'
