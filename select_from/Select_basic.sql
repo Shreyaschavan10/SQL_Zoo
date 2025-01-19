bcbb5cd547a5afa7a0c53193ffed98b036fd0526
Shreyaschavan10 <166787514+Shreyaschavan10@users.noreply.github.com>
2025-01-19 17:02:32 +0530

    Solved problems

--- select_from/Select_basic.sql
+++ select_from/Select_basic.sql
@@ -1,3 +1,7 @@
 -- Que 1
 -- The example uses a WHERE clause to show the population of 'Germany'. Note that strings should be in 'single quotes';
 select population from world where name = 'Germany'
+
+-- Ques 2 
+-- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+select name, population from world where name IN ('Sweden', 'Norway', 'Denmark')
