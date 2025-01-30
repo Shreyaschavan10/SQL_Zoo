476c51e8955c498f965aba272d46ffc984e82fb7
Shreyaschavan10 <166787514+Shreyaschavan10@users.noreply.github.com>
2025-01-30 18:50:43 +0530

    Update SELECT within SELECT.sql

--- SELECT within SELECT/SELECT within SELECT.sql
+++ SELECT within SELECT/SELECT within SELECT.sql
@@ -17,3 +17,16 @@ WHERE continent = ('Europe') AND gdp / population > (
 		FROM world
 		WHERE name IN ('United kingdom')
 		)
+
+
+-- 3. List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+
+SELECT name,
+	continent
+FROM world
+WHERE continent IN (
+		SELECT continent
+		FROM world
+		WHERE name IN ('Argentina', 'Australia')
+		)
+ORDER BY name
