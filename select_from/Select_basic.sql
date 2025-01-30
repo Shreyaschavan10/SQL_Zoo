5f1937275b448a3ffd4fc3b071fa8244a49bb2bf
Shreyaschavan10 <166787514+Shreyaschavan10@users.noreply.github.com>
2025-01-30 18:45:55 +0530

    Update Select_basic.sql

--- select_from/Select_basic.sql
+++ select_from/Select_basic.sql
@@ -5,3 +5,9 @@ select population from world where name = 'Germany'
 -- Ques 2 
 -- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
 select name, population from world where name IN ('Sweden', 'Norway', 'Denmark')
+
+-- show the country and the area for countries with an area between 200,000 and 250,000.
+SELECT name, area
+FROM world
+WHERE area BETWEEN 200000
+		AND 250000
