ddbe7a41cba008b2506709d4596738f83d90ff34
Shreyaschavan10 <166787514+Shreyaschavan10@users.noreply.github.com>
2025-02-05 21:39:36 +0530

    Update Joins.sql

--- JOINS/Joins.sql
+++ JOINS/Joins.sql
@@ -10,3 +10,14 @@ SELECT matchid,
 	player
 FROM goal
 WHERE teamid = 'GER'
+
+
+-- Q2 
+-- Show id, stadium, team1, team2 for just game 1012
+
+SELECT id,
+	stadium,
+	team1,
+	team2
+FROM game
+WHERE id = '1012'
