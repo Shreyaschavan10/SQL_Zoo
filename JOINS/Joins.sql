4a5017cdc74ede9b9d6468ddebcb7a31a5cd22f1
Shreyaschavan10 <166787514+Shreyaschavan10@users.noreply.github.com>
2025-02-05 21:37:33 +0530

    Update Joins.sql

--- JOINS/Joins.sql
+++ JOINS/Joins.sql
@@ -1,2 +1,8 @@
 -- This tutorial introduces JOIN which allows you to use data from two or more tables. 
 -- The tables contain all matches and goals from UEFA EURO 2012 Football Championship in Poland and Ukraine.
+
+--Q 1.
+-- The first example shows the goal scored by a player with the last name 'Bender'. 
+--The * says to list all the columns in the table - a shorter way of saying matchid, teamid, player, gtime
+--Modify it to show the matchid and player name for all goals scored by Germany. To identify German players, check for: teamid = 'GER'
+
